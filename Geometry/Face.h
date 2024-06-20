95b674bd304fb87a655f493e4ac3ee3bc1022e69
PARAG\parag <31978917+paragpathak2006@users.noreply.github.com>
2024-06-20 09:43:52 -0400

    test

--- Geometry/Face.h
+++ Geometry/Face.h
@@ -8,6 +8,7 @@ public:
 	Face(int i,int j,int k) { v[0] = i; v[1] = j; v[2] = k; }
 	Face(int _v[3]) { v[0] = _v[0]; v[1] = _v[1]; v[2] = _v[2]; }
 
+
 //	double dist(const Point& target, const Point* points) const;
 };
 
